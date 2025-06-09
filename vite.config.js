237eae5c1afca723a91bad3c665594a6a77a0078
humamT <humam.01@hotmail.com>
2025-06-09 11:02:12 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Project-12/", // <-- ADDED THIS LINE GITHUB PAGES
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'gh-pages' ? '/Project-12/' : '/', // <-- ADDED THIS LINE GITHUB PAGES AND NETLIFY
+  }
 })
